b6b448ab1e884d2f83b365542b0f1654307f5618
bill-i-am-code <69911640+bill-i-am-code@users.noreply.github.com>
2021-02-26 15:36:57 +0000

    Added EEPROM to store target length

--- cableMeasure.ino
+++ cableMeasure.ino
@@ -1,20 +1,34 @@
+/* 
+Arduino device to automatically measure cable length pulled. 
+Bill Addison
+26 February 2021
+*/
+
+
 #include <LiquidCrystal.h>
+#include <EEPROM.h>
+
 LiquidCrystal lcd (12, 11, 2, 3, 4, 5);
 
-const int encPin2 = 10;
-const int encPin1 = 9;
-const int switchPin1 = 6;
-const int switchPin2 = 7;
-const int switchPin3 = 8;
-const int targetLengthIndicator = 13;
+//constants to define which pins or the arduino are used for what.
+const int encPin1 = 9; //rotary encoder attached to measuring wheel CLK
+const int encPin2 = 10; //rotary encoder attached to measuring wheel DATA
+const int switchPin1 = 6; //Switch 1 - decrease the target length
+const int switchPin2 = 7; //Switch 2 - increase the target length
+const int switchPin3 = 8; //Switch 3 - reset the measured length to 0
+const int targetLengthIndicator = 13; //output pin used to indicate the measured length is a multiple of the target length
 
-int pos = 0; 
+
+// variables to analyse the rotary encoder position
+int pos = 0; //this will increment (or decrement) by 1 for each clockwise (counterclockwise) "click" of the rotary encoder
 int state;
 int lastState;
 
-int targetLength = 0;
+// variables to store measured and target length
+int targetLength;
 int measuredLength = 0;
 
+//variable to analyse and debounce the three switch inputs
 int switchState1;
 int switchState2;
 int switchState3;
@@ -26,20 +40,26 @@ unsigned long lastDebounceTime2 = 0;
 unsigned long lastDebounceTime3 = 0;  
 unsigned long debounceDelay = 50;
 
+//constants used to convert 'pos' variable into a length
 const int C = 1; //circumference of measuring wheel
 const int N = 1; //number of "clicks" for one revolution of wheel
 
 
 void setup(){
+    //define pinModes
     pinMode (encPin1 ,INPUT);
     pinMode (encPin2 ,INPUT);
     pinMode (switchPin1 ,INPUT);
     pinMode (switchPin2 ,INPUT);
     pinMode (switchPin3 ,INPUT);
     pinMode (targetLengthIndicator, OUTPUT);
+
+    //read initial values required.
     lastState = digitalRead(encPin1); 
     digitalWrite(targetLengthIndicator, LOW);
+    targetLength =  EEPROM.read(0);
 
+    //initilise LCD display
     lcd.begin(16, 2);
     lcd.print("Cable Measure");
     // lcd.setCursor(0,1);
@@ -51,8 +71,7 @@ void setup(){
 
 void loop(){
 
-    //read rotary encoder
-
+    //read rotary encoder and increment or decrement  position accordingly
     state = digitalRead(encPin1);
     if (state != lastState){     
         if (digitalRead(encPin2) != state) { 
@@ -68,7 +87,7 @@ void loop(){
 
     measuredLength = pos*C/N;
 
-    //read from switch 1
+    //read from switch 1 and action
 
     int s1 = digitalRead(switchPin1);
     if (s1 != lastSwitchState1){
@@ -79,12 +98,13 @@ void loop(){
             switchState1 = s1;
             if(switchState1 == HIGH && targetLength > 0){
                 targetLength--;
+                EEPROM.write(0,targetLength);
             }
         }
     } 
     lastSwitchState1 = s1;
 
-    //read from switch 2
+    //read from switch 2 and action
 
     int s2 = digitalRead(switchPin2);
     if (s2 != lastSwitchState2){
@@ -95,12 +115,13 @@ void loop(){
             switchState2 = s2;
             if(switchState2 == HIGH){
                 targetLength++;
+                EEPROM.write(0,targetLength);
             }
         }
     } 
     lastSwitchState2 = s2;
 
-    //read from switch 3
+    //read from switch 3 action
     
     int s3 = digitalRead(switchPin3);
     if (s3 != lastSwitchState3){
@@ -110,7 +131,7 @@ void loop(){
         if (s3 != switchState3) {
             switchState3 = s3;
             if(switchState3 == HIGH){
-                targetLength = 0;
+                pos = 0;
             }
         }
     } 
