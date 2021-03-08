9adf2953caa17fd338ac86f7516922eb5980071b
bill-i-am-code <69911640+bill-i-am-code@users.noreply.github.com>
2021-03-08 14:28:45 +0000

    Added press and hold, and 10+/- buttons

--- cableMeasureVMA203/cableMeasureVMA203.ino
+++ cableMeasureVMA203/cableMeasureVMA203.ino
@@ -7,7 +7,7 @@ Bill Addison
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
 
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+//**** EDIT THE CONTENTS BELOW FOR YOUR CONFIGURATION ***********************************************************************
 
 //constants to define which pins or the arduino are used for what.
 const int encPin1 = 2;   //rotary encoder attached to measuring wheel CLK
@@ -16,7 +16,17 @@ const int switchPin = 0; //pin that resitor ladder and switches are connected to
 const int backLightPin = 10;
 const int targetLengthIndicator = 13; //output pin used to indicate the measured length is a multiple of the target length
 
+//constants used relating to display brightness and button debounce + repeat.
 const int backLightBrightness = 100; //set the LCD backlight brightness between 0 and 255
+const int debounceDelay = 50;        // the debounce time in ms
+int actionrepeat = 350;              // the time in ms which holding a button repeats. Set to 0 for no repeat.
+
+//constants used to convert 'pos' variable into a length
+const float C = 1.0;         //circumference of measuring wheel
+const int N = 10;            //number of "clicks" for one revolution of wheel
+char unitOfLength[3] = " m"; //specify the unit of length (2 characters), and use the same unit of length used to specify C
+
+//***************************************************************************************************************************
 
 // variables to analyse the rotary encoder position
 int pos = 0; //this will increment (or decrement) by 1 for each clockwise (counterclockwise) "click" of the rotary encoder
@@ -27,6 +37,7 @@ int lastState;
 int targetLength;
 float measuredLength = 0;
 
+//constants relating to Vellemann VMA203
 int lcd_key = 0;
 int adc_key_in = 0;
 #define btnRIGHT 0
@@ -35,18 +46,15 @@ int adc_key_in = 0;
 #define btnLEFT 3
 #define btnSELECT 4
 #define btnNONE 5
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
 int buttonState;         // the current reading from the input pin
 int lastButtonState = 5; // the previous reading from the input pin
 int tmpButtonState = 5;
 long lastDebounceTime = 0;
-long debounceDelay = 100; // the debounce time
-int actioned = 0;
-
-//constants used to convert 'pos' variable into a length
-const float C = 1.0; //circumference of measuring wheel
-const int N = 10;    //number of "clicks" for one revolution of wheel
-
-char unitOfLength[3] = " m"; //specify the unit of length (2 characters), and use the same unit of length used to specify C
+int actioned = 0; // 1 if a button has been pressed and the result actioned. Returns to 0 when no button pressed.
+long lastactioned;
 
 // read the buttons
 int read_LCD_buttons()
@@ -58,7 +66,7 @@ int read_LCD_buttons()
     if (adc_key_in > 1000)
         return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
     // For V1.1 us this threshold
-    if (adc_key_in < 50)
+    /*  if (adc_key_in < 50)
         return btnRIGHT;
     if (adc_key_in < 250)
         return btnUP;
@@ -67,9 +75,9 @@ int read_LCD_buttons()
     if (adc_key_in < 650)
         return btnLEFT;
     if (adc_key_in < 850)
-        return btnSELECT;
+        return btnSELECT; */
     // For V1.0 comment the other threshold and use the one below:
-    /*
+
     if (adc_key_in < 50)
         return btnRIGHT;
     if (adc_key_in < 195)
@@ -80,7 +88,7 @@ int read_LCD_buttons()
         return btnLEFT;
     if (adc_key_in < 790)
         return btnSELECT;
-    */
+
     return btnNONE; // when all others fail, return this...
 }
 
@@ -89,7 +97,6 @@ void setup()
     //define pinModes
     pinMode(encPin1, INPUT);
     pinMode(encPin2, INPUT);
-
     pinMode(targetLengthIndicator, OUTPUT);
 
     //read initial values required.
@@ -102,12 +109,10 @@ void setup()
     lcd.begin(16, 2);
     lcd.print("Cable Measure");
     lcd.setCursor(0, 1);
-    lcd.print("V1.00 210225");
+    lcd.print("V1.10 210308");
     delay(2000);
     lcd.setCursor(0, 0);
     lcd.print(" ACTUAL  TARGET ");
-
-    Serial.begin(9600);
 }
 
 // main loop
@@ -134,41 +139,60 @@ void loop()
     // save the reading.  Next time through the loop,
     // it'll be the lastButtonState:
     lastButtonState = tmpButtonState;
-
-    if (buttonState == 1 && actioned == 0)
+    // Up button, 1, increase target length by 1
+    if (buttonState == 1 && (actioned == 0 || (actionrepeat > 0 && millis() - lastactioned > actionrepeat)))
     {
         actioned = 1;
+        lastactioned = millis();
         targetLength++;
         EEPROM.update(0, targetLength);
     }
-    if (buttonState == 2 && actioned == 0)
+    // Down button, 2, decrease target length by 1
+    if (buttonState == 2 && (actioned == 0 || (actionrepeat > 0 && millis() - lastactioned > actionrepeat)))
     {
         actioned = 1;
+        lastactioned = millis();
         if (targetLength > 0)
         {
             targetLength--;
         }
         EEPROM.update(0, targetLength);
     }
-    if (buttonState == 3 && actioned == 0)
+    // Right button, 0, increase target length by 10
+    if (buttonState == 0 && (actioned == 0 || (actionrepeat > 0 && millis() - lastactioned > actionrepeat)))
     {
         actioned = 1;
+        lastactioned = millis();
+        targetLength = targetLength + 10;
+        EEPROM.update(0, targetLength);
+    }
+    // Left button, 3, decrease target length by 10
+    if (buttonState == 3 && (actioned == 0 || (actionrepeat > 0 && millis() - lastactioned > actionrepeat)))
+    {
+        actioned = 1;
+        lastactioned = millis();
+        if (targetLength > 10)
+        {
+            targetLength = targetLength - 10;
+        }
+        else
+            targetLength = 0;
+        EEPROM.update(0, targetLength);
+    }
+    // Select button, 4, reset actual length
+    if (buttonState == 4 && (actioned == 0 || (actionrepeat > 0 && millis() - lastactioned > actionrepeat)))
+    {
+        actioned = 1;
+        lastactioned = millis();
         pos = 0;
     }
     if (buttonState == 5)
     {
         actioned = 0;
     }
-
-    /*    Serial.print(tmpButtonState);
-    Serial.print("\t");
-    Serial.print(lastButtonState);
-    Serial.print("\t");
-    Serial.println(buttonState); */
-
     lastButtonState = tmpButtonState;
 
-    //read rotary encoder and increment or decrement  position accordingly
+    //read rotary encoder and increment or decrement position accordingly
     state = digitalRead(encPin1);
     if (state != lastState)
     {
